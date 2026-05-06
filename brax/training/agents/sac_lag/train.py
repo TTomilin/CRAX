e184b9498338b940e53cd9b33b46022eb4853411
TTomilin <tristan.tomilin@hotmail.com>
2026-05-06 15:47:42 +0200

    Default to 0 evals

--- brax/training/agents/sac_lag/train.py
+++ brax/training/agents/sac_lag/train.py
@@ -144,7 +144,7 @@ def train(
     discounting: float = 0.99,
     seed: int = 0,
     batch_size: int = 256,
-    num_evals: int = 1,
+    num_evals: int = 0,
     normalize_observations: bool = False,
     max_devices_per_host: Optional[int] = None,
     reward_scaling: float = 1.0,
